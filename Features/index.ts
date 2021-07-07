b4de6df3ddf45f62ea4ee0ddd4744a155eefe721
Mitchell <mwhyuen@gmail.com>
2021-07-07 15:17:06 +0200

    feat: add features. explains type annotations

---
+++ Features/index.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20
+}
+
+class Color{
+  constructor(public color: string){
+
+  }
+}
+
+const white = new Color("#fff");
+white;
